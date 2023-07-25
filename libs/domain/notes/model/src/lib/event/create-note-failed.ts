a3318d8952520aade34184ff01dbf88061e1ea72
Zebedee Lewis <zebedeeslewis@gmail.com>
2023-07-25 13:28:05 -0600

    feat(domain-notes--model): add reason to create not failed event

--- libs/domain/notes/model/src/lib/event/create-note-failed.ts
+++ libs/domain/notes/model/src/lib/event/create-note-failed.ts
@@ -1,24 +1,30 @@
 import { ImmutableModel } from '@notes/domain/shared/immutable-model'
-import { Time, Id } from '@notes/domain/shared/value-object'
+import { Time, Id, Select } from '@notes/domain/shared/value-object'
 
 import TaggedModel = ImmutableModel.TaggedModel
 import factory = ImmutableModel.factory
 
 export module CreateNoteFailedEvent {
-  export interface ModelI
+  export type Reason
+    = 'AuthenticationFailure'
+    | 'UnauthorizedAction'
+
+  interface ModelI
     { _tag: 'CreateNoteFailedEvent'
     , id: Id.Value
     , command: Id.Value
+    , reason: Select.Options<Reason>
     , event_time: Time.Value
     , }
   
   export type Model
     = TaggedModel<'CreateNoteFailedEvent', ModelI>
   
-  export const DEFAULT_VALUE: ModelI
+  const DEFAULT_VALUE: ModelI
     = { _tag: 'CreateNoteFailedEvent'
       , id: Id.__unsafe_of('f77d466a-2993-11ee-be56-0242ac120002')
       , command: Id.__unsafe_of('f77d466a-2993-11ee-be56-0242ac120002')
+      , reason: Select.__unsafe_of<Reason>('AuthenticationFailure')
       , event_time: Time.__unsafe_of(new Date())
       , }
   
