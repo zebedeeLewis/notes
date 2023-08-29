97aabc56923dba698b65d3d8de477e3c5aaa080f
Zebedee Lewis <zebedeeslewis@gmail.com>
2023-08-29 12:53:16 -0600

    WIP

--- libs/utils/tagged-record/src/index.ts
+++ libs/utils/tagged-record/src/index.ts
@@ -9,8 +9,10 @@ export import Accessors = accessors.Accessors
 export import Factory = tagged_record.Factory
 
 export import s = utils.s
+export import match = utils.match
 export import mkFactory = tagged_record.mkFactory
 export import isTaggedRecord = tagged_record.isTaggedRecord
 
 export import _tagged_record_examples = tagged_record.examples
 export import _accessors_examples = accessors.examples
+export import _utils_examples = utils.examples
